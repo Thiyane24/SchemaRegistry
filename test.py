180da976db52db1e2537067e01e82663db61cc1b
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-03-25 04:54:47 +0800

    test.py

--- test.py
+++ test.py
@@ -5,10 +5,15 @@ from SchemaRegistry.field import Field
 
 field1 = Field("Age", "int", True, None)
 schema1 = Schema("Schema1", [field1], 1)
+field2 = Field("Name", "str", False, None)
+schema2 = Schema("Schema1", [field2], 2)
 
 
 schema_registry= SchemaRegistry()
 
 schema_registry.register(schema1.name, schema1)
+schema_registry.register(schema2.name, schema2)
+print(schema_registry.get("Schema1"))
 
 print(schema_registry.list_versions("Schema1"))
+print(schema_registry.list_schemas())
